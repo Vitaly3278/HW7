15b723569adf8cebe76d6cc3f724d3bc528ce5c4
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-21 19:40:08 +0300

    Average ariphmetic

---
+++ Work3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+void GetArray (int rows, int colums)
+{
+    int [,] array = new int [rows, colums]; 
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i, j] = new Random().Next(0,10);
+            Console.Write($"{array [i, j]} ");
+        }
+        Console.WriteLine();
+
+    }
+    Console.WriteLine("Среднее арифметическое элементов в каждом столбце:");
+    
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sum =0;
+        for (int j = 0; j< array.GetLength(0); j++)
+        {
+            sum += array [j, i];
+        }
+        
+        Console.Write($"{Math.Round(sum / array.GetLength(0),2)};  ");
+    }
+    
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+GetArray(rows, colums);
